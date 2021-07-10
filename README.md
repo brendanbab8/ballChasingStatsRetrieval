fffb3b9e47e6f8e3a2cc1798ae883e74c1b90586
brendanbab <bab356@cornell.edu>
2021-07-10 18:03:11 -0400

    Group Stat Retrieval Success -Added debug flag for console prints.

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 ## Running prerequisites
 1. Runs on Windows, no guarantees about Macs
 2. You need Python 3.9.6 installed. Find the download link [here](https://www.python.org/downloads/)
-3. After cloning this repo, run this command from cmd: `py script.py`. You will your API token from the site.
+3. After obtaining the source code, run this command from the command prompt: `py script.py`. You will need your API token from the site.
 
 ## Current Capabilities
 1. Retrieves all groups created by Bricks and prints them to the command line.
+2. Parses the group data to retrieve the group IDs
--- script.py
+++ script.py
@@ -3,9 +3,47 @@ import subprocess as sp
 import json
 
 STEAMID = "76561199061858917"
+debug = True
+'''
+Communicates with ballchasing.com to retrieve the group stats.
+Returns a list of group stats.
+'''
+def retrieve_stats():
+  token = gp.getpass("Enter your authorization token: ")
 
-token = gp.getpass("Enter your authorization token: ")
+  print("\n", 
+    "Executing request: \"https://ballchasing.com/api/groups?SteamID64=" + 
+    STEAMID + "\"")
+  response = sp.check_output("curl -s -H Authorization:" + token + 
+    " \"https://ballchasing.com/api/groups?creator=" + STEAMID + "\"", 
+    text=True)
+  res_obj = json.loads(response)["list"]
 
-response = sp.check_output("curl -s -H Authorization:" + token + " \"https://ballchasing.com/api/groups?creator=" + STEAMID + "\"", text=True)
-print("\n", "Executing request: \"https://ballchasing.com/api/groups?SteamID64=" + STEAMID + "\"")
-print("\n", json.dumps(json.loads(response), indent=4))
+  if debug:
+    print("\n", json.dumps(res_obj, indent=4))
+
+  groups = []
+
+  for group in res_obj:
+    groups.append(group["id"])
+
+  if debug:
+    print("\n", groups) 
+
+  stats = []
+
+  for id in groups:
+    print("\n", "Executing request: \"https://ballchasing.com/api/groups/" + 
+      id + "\"")
+    response = sp.check_output("curl -s -H Authorization:" + token + 
+      " \"https://ballchasing.com/api/groups/" + id + "\"", text=True)
+    stats.append(json.loads(response))
+  
+  return stats
+'''
+Script execution
+'''
+stats = retrieve_stats()
+
+if debug:
+  print("\n", json.dumps(stats, indent=4))
